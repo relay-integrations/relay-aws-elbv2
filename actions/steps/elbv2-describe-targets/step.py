188596bbe5c895f147b8cf8c2c6a8be321f499f4
Kenaz <kenaz@puppetlabs.com>
2020-06-12 21:30:55 -0700

    Adding STS support

--- actions/steps/elbv2-describe-targets/step.py
+++ actions/steps/elbv2-describe-targets/step.py
@@ -4,10 +4,17 @@ from nebula_sdk import Interface, Dynamic as D
 
 relay = Interface()
 
+session_token = None
+try:
+  session_token = relay.get(D.aws.connection.sessionToken)
+except:
+  pass
+
 sess = boto3.Session(
   aws_access_key_id=relay.get(D.aws.connection.accessKeyID),
   aws_secret_access_key=relay.get(D.aws.connection.secretAccessKey),
   region_name=relay.get(D.aws.region),
+  aws_session_token=session_token
 )
 
 elbv2 = sess.client('elbv2')
